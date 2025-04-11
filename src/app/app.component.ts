037f591dd4e7f33be58fa8607f93a933842a9c8d
max liu <max.liu@digitimes.com>
2025-04-11 17:13:07 +0800

    feat: add summary feature with new components and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SummeryListComponent} from './shared/components/summery-list/summery-list.component';
+import { FormsModule } from '@angular/forms';
+import { Select } from 'primeng/select';
+import {SummeryService} from './summery/summery.service';
+import {ContainerComponent} from './shared/components/container/container.component';
+import {Button} from 'primeng/button';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SummeryListComponent, FormsModule, ContainerComponent, Button],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'summary-report';
+
 }
---
+++ src/domain/summery.ts
@@ -0,0 +1,11 @@
+export interface Summery {
+  company: string;
+  quarter: string;
+  original: string;
+  summery: string;
+}
+
+export interface Company {
+  name: string;
+  code: string;
+}
