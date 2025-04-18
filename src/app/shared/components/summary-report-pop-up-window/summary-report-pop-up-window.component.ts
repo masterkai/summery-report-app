ad7d48832a8bef1bbf5d7e624b8578d71d8063f2
max liu <max.liu@digitimes.com>
2025-04-18 16:27:54 +0800

    feat: Add summary report pop-up window component

---
+++ src/app/shared/components/summary-report-pop-up-window/summary-report-pop-up-window.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-summary-report-pop-up-window',
+  imports: [],
+  templateUrl: './summary-report-pop-up-window.component.html',
+  styleUrl: './summary-report-pop-up-window.component.css'
+})
+export class SummaryReportPopUpWindowComponent {
+
+}
